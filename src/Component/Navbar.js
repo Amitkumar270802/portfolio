372ea80bd297d2b9bbd0ddf0697821d6a7549f3a
Amit Kumar <amitkumar270802@gmail.com>
2023-12-13 15:42:14 +0530

    Update Navbar.js

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -8,78 +8,74 @@ const Navbar = () => {
     setClick(false);
   };
   return (
-    <div className="relative w-fit ">
-      <div className=" fixed top-0 w-full flex justify-center  bg-black ">
-        <div className=" flex w-7/12 mx-auto p-4 items-center md:justify-between  text-white bg-black z-10">
-            <h1 className="lg:block hidden font-bold">Amit </h1>
-          {/* <div className=" p-2"> */}
-            <ul className="flex gap-x-4 w-fit justify-center items-center">
-              <li className="nav-item cursor-pointer hover:text-[#e8c768]">
-                <Link
-                  to="home"
-                  spy={true}
-                  activeStyle={{color:"#e8c768"}}
-                  smooth={true}
-                  offset={-900}
-                  duration={500}
-                  onClick={closeMenu}
-                >
-                  About
-                </Link>
-              </li>
-              <li className="nav-item cursor-pointer hover:text-[#e8c768]">
-                <Link
-                  to="skills"
-                  spy={true}
-                  smooth={true}
-                  offset={-100}
-                  duration={500}
-                  onClick={closeMenu}
-                >
-                  Skills
-                </Link>
-              </li>
-              <li className="nav-item cursor-pointer hover:text-[#e8c768]">
-                <Link
-                  to="qualifications"
-                  spy={true}
-                  smooth={true}
-                  offset={-130}
-                  duration={500}
-                  onClick={closeMenu}
-                >
-                  Qualifications
-                </Link>
-              </li>
-              <li className="nav-item cursor-pointer hover:text-[#e8c768]">
-                <Link
-                  to="project"
-                  spy={true}
-                  smooth={true}
-                  offset={-120}
-                  duration={500}
-                  onClick={closeMenu}
-                >
-                  Projects
-                </Link>
-              </li>
-              <li className="w-fit nav-item cursor-pointer hover:text-[#e8c768]">
-                <Link
-                  to="contact"
-                  spy={true}
-                  smooth={true}
-                  offset={-100}
-                  duration={500}
-                  onClick={closeMenu}
-                >
-                  Contact Us
-                </Link>
-              </li>
-            </ul>
+    <div className="relative w-full overflow-hidden flex justify-center items-center bg-black text-white">
+      <div className="flex fixed top-0 bg-black items-center sm:justify-center md:justify-between qs:min-w-8/12 w-9/12 md:w-7/12  h-[50px] ">
+        <div className="sm:hidden md:inline-block p-2">Amit</div>
+        <div className="flex gap-x-3 md:gap-x-4 justify-center items-center ">
+          <div className="nav-item cursor-pointer hover:text-[#e8c768] ">
+            <Link
+              to="home"
+              spy={true}
+              activeStyle={{ color: "#e8c768" }}
+              smooth={true}
+              offset={-900}
+              duration={500}
+              onClick={closeMenu}
+            >
+              About
+            </Link>
+          </div>
+          <div className="nav-item cursor-pointer hover:text-[#e8c768] ">
+            <Link
+              to="skills"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+              onClick={closeMenu}
+            >
+              Skills
+            </Link>
+          </div>
+          <div className="nav-item cursor-pointer hover:text-[#e8c768] ">
+            <Link
+              to="qualifications"
+              spy={true}
+              smooth={true}
+              offset={-130}
+              duration={500}
+              onClick={closeMenu}
+            >
+              Qualifications
+            </Link>
+          </div>
+          <div className="nav-item cursor-pointer hover:text-[#e8c768] ">
+            <Link
+              to="project"
+              spy={true}
+              smooth={true}
+              offset={-120}
+              duration={500}
+              onClick={closeMenu}
+            >
+              Projects
+            </Link>
+          </div>
+          <div className=" w-fit  nav-item cursor-pointer hover:text-[#e8c768] ">
+            <Link
+              to="contact"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+              onClick={closeMenu}
+            >
+              <div>Contact Us</div>
+            </Link>
           </div>
         </div>
       </div>
-    // </div>
+    </div>
   );
 };
 
