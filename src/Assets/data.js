60937d4bffddbdf8f2baac69946af31ffdb5d040
Amit Kumar <amitkumar270802@gmail.com>
2023-12-19 00:57:10 +0530

    Update data.js

--- src/Assets/data.js
+++ src/Assets/data.js
@@ -3,6 +3,7 @@ import Movie from "./Movie.png";
 import Rozarpay from "./Rozarpay.png";
 import Weather from './weather.png'
 import Portfolio from './portfolio.png'
+import Blog from './Blog.png'
 
 const data = [
   { id: 0,  description: "",   repo:"",   liveLink:"https://movie-app-amitkumar270802.vercel.app/",               image: Movie,    name: "Movie Website",   Class: 5 },
@@ -10,5 +11,7 @@ const data = [
   { id: 2,  description: "",   repo:"",   liveLink:"https://shopping-site-lvvsrx9u9-amitkumar270802.vercel.app/", image: Cart,     name: "Shopping Cart",   Class: 5 },
   { id: 3,  description: "",   repo:"",   liveLink:"https://weather-app-pied-sigma.vercel.app/",                  image:Weather,   name: "Weather Website", Class: 5 },
   { id: 4,  description: "",   repo:"",   liveLink:"https://portfolio-amitkumar270802.vercel.app/",               image:Portfolio, name: "Portfolio",       Class: 5 },
+  { id: 5,  description: "",   repo:"",   liveLink:"https://blog-app-amitkumar270802.vercel.app/",                image:Blog,      name: "Blog Website",    Class: 5 },
+  
 ];
 export default data;
