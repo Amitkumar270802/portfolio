93b064a6e57041fea132f1e66a8c0a3e15c41b81
Amit Kumar <amitkumar270802@gmail.com>
2024-01-07 18:08:40 +0530

    Update Navbar.js

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-scroll";
 
 const Navbar = () => {
@@ -7,85 +7,86 @@ const Navbar = () => {
   const closeMenu = () => {
     setClick(false);
   };
+
   return (
-    <div className="relative  flex justify-center items-center bg-black text-white z-10 ">
-      <div className="flex justify-center bg-black w-screen border  ">
-        <div className="flex fixed top-0 bg-black  items-center sm:justify-center lg:justify-between qs:min-w-8/12 w-9/12 md:w-7/12  h-[50px] ">
-          <Link to="home"
-                spy={true}
-                activeStyle={{ color: "#e8c768" }}
-                smooth={true}
-                duration={1000}
-                onClick={ ()=> window.location.reload(false)}>
-            <div className="hidden lg:inline-block p-2 cursor-pointer">Amit</div>
-          </Link>
-          <div className="flex gap-x-3 md:gap-x-4 justify-center items-center ">
-            <div className="nav-item cursor-pointer hover:text-[#e8c768] ">
-              <Link
-                to="home"
-                spy={true}
-                activeStyle={{ color: "#e8c768" }}
-                smooth={true}
-                offset={-120}
-                duration={1000}
-                onClick={closeMenu}
-              >
-                About
-              </Link>
-            </div>
-            <div className="nav-item cursor-pointer hover:text-[#e8c768] ">
-              <Link
-                to="skills"
-                spy={true}
-                smooth={true}
-                offset={-100}
-                duration={500}
-                onClick={closeMenu}
-              >
-                Skills
-              </Link>
-            </div>
-            <div className="nav-item cursor-pointer hover:text-[#e8c768] ">
-              <Link
-                to="qualifications"
-                spy={true}
-                smooth={true}
-                offset={-130}
-                duration={500}
-                onClick={closeMenu}
-              >
-                Qualifications
-              </Link>
-            </div>
-            <div className="nav-item cursor-pointer hover:text-[#e8c768] ">
-              <Link
-                to="project"
-                spy={true}
-                smooth={true}
-                offset={-120}
-                duration={500}
-                onClick={closeMenu}
-              >
-                Projects
-              </Link>
-            </div>
-            <div className=" w-fit  nav-item cursor-pointer hover:text-[#e8c768] ">
-              <Link
-                to="contact"
-                spy={true}
-                smooth={true}
-                offset={-100}
-                duration={500}
-                onClick={closeMenu}
-              >
-                <div>Contact</div>
-              </Link>
-            </div>
+    <div className=" relative bg-black mx-auto border border-white z-10 ">
+      <div className="flex justify-around items-center fixed top-0 shadow-[100px_0px_1000px_-14px_#ffffff] bg-black h-[60px] w-[100vw] ">
+
+        <Link
+          to="home"
+          spy={true}
+          activeStyle={{ color: "#e8c768" }}
+          smooth={true}
+          duration={1000}
+          onClick={() => window.location.reload(false)}
+        >
+          <div className="hidden lg:inline-block p-2 cursor-pointer text-white border border-[#21211f] rounded-md ">Amit</div>
+        </Link>
+        <div className="flex gap-x-3 md:gap-x-4 justify-center items-center text-white">
+          <div className="nav-item cursor-pointer hover:text-[#e8c768] border-b border-[#21211f] rounded-md p-2">
+            <Link
+              to="home"
+              spy={true}
+              activeStyle={{ color: "#e8c768" }}
+              smooth={true}
+              offset={-120}
+              duration={1000}
+              onClick={closeMenu}
+            >
+              About
+            </Link>
+          </div>
+          <div className="nav-item cursor-pointer hover:text-[#e8c768] border-b border-[#21211f] rounded-md p-2">
+            <Link
+              to="skills"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+              onClick={closeMenu}
+            >
+              Skills
+            </Link>
+          </div>
+          <div className="nav-item cursor-pointer hover:text-[#e8c768] border-b border-[#21211f] rounded-md p-2">
+            <Link
+              to="qualifications"
+              spy={true}
+              smooth={true}
+              offset={-130}
+              duration={500}
+              onClick={closeMenu}
+            >
+              Qualifications
+            </Link>
+          </div>
+          <div className="nav-item cursor-pointer hover:text-[#e8c768] border-b border-[#21211f] rounded-md p-2 ">
+            <Link
+              to="project"
+              spy={true}
+              smooth={true}
+              offset={-120}
+              duration={500}
+              onClick={closeMenu}
+            >
+              Projects
+            </Link>
+          </div>
+          <div className=" w-fit  nav-item cursor-pointer hover:text-[#e8c768] border-b border-[#21211f] rounded-md p-2">
+            <Link
+              to="contact"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+              onClick={closeMenu}
+            >
+              <div>Contact</div>
+            </Link>
           </div>
         </div>
       </div>
     </div>
   );
 };
-
 export default Navbar;
